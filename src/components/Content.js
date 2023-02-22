0afa7782fa4581e79f90123e7fbdeda951db536b
Synnehe <synnehei@hotmail.no>
2023-02-22 17:24:10 +0100

    Added basic html to components

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,5 +1,10 @@
-export default function Content() {
+
+export default function Content({contentTitle, description, resourceList}) {
   return(
-    
+    <section>
+    <h2 id="content-title">{contentTitle}</h2>
+    <p id="description">{description}</p>
+    <ul id="resource-list">{resourceList}</ul>
+  </section>
   )
 }
